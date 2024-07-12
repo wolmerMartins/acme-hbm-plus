ea9112f9e818ce536c14087a3927d72221bd8836
Wolmer Martins <wolmermartins@hotmail.com>
2024-07-12 17:26:49 -0300

    feat: creates service to create profile

--- src/modules/profiles/profiles.module.ts
+++ src/modules/profiles/profiles.module.ts
@@ -3,6 +3,9 @@ import { Module } from '@nestjs/common'
 import { IProfilesRepository } from './domain/ports/profiles.repository'
 import { ProfilesMongoDB } from './repositories/profiles.mongodb'
 import { ProfileModel } from './repositories/models/profile.model'
+import { ProfilesController } from './controllers/profiles.controller'
+import { IProfilesService } from './domain/ports/profiles.service'
+import { ProfilesService } from './domain/services/profiles'
 
 @Module({
   providers: [
@@ -13,7 +16,15 @@ import { ProfileModel } from './repositories/models/profile.model'
           new ProfileModel()
         )
       }
+    },
+    {
+      provide: IProfilesService,
+      useFactory() {
+        return new ProfilesService(...arguments as unknown as [IProfilesRepository])
+      },
+      inject: [IProfilesRepository]
     }
-  ]
+  ],
+  controllers: [ProfilesController]
 })
 export class ProfilesModule {}
